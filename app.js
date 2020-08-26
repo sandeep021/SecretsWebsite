6d0a0f1f40c1a6e1da0136f347215121d5962b93
Sandeep Yadva <41124504+sandeep021@users.noreply.github.com>
2020-08-26 12:58:54 +0530

    level 4 security
    added (salting + hash) security using bcrypt

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser= require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
-const md5 = require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
+//const md5 = require("md5");
 mongoose.connect('mongodb://localhost:27017/userBD', {useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
@@ -34,39 +35,51 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
   //console.log(req.body);
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(err) {
-      console.log(err);
-    }
-    else {
-      res.render("secrets");
-    }
-  })
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+
+    });
+
+    newUser.save(function(err){
+      if(err) {
+        console.log(err);
+      }
+      else {
+        res.render("secrets");
+      }
+    });
+
+});
+
+
 });
 
 app.post("/login", function(req, res){
   const enteredusername= req.body.username;
-  const userpassword= md5(req.body.password);
+  const userpassword= (req.body.password);
   User.findOne({email: enteredusername}, function(err, foundUser){
     if(err){
       console.log(err);
     }
     else{
-      if(foundUser.password === userpassword){
+      bcrypt.compare(userpassword, foundUser.password, function(err, result) {
+    // result == true
+        if(result==true){
             res.render("secrets");
-      }
-      else {
-        console.log("invalid pass");
-      }
+        }
+        else {
+          console.log("invalid pass");
+        }
+       });
+    }
       // console.log(enteredusername, " -- 1");
       // console.log(foundUser.email, " -- 2");
       // console.log(foundUser.password, " -- 3");
       // console.log(userpassword, " -- 4");
-    }
+
   });
 });
 
