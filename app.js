f2e62f654c76bc7eb2d61981c2859728b8a0809d
Sandeep Yadva <41124504+sandeep021@users.noreply.github.com>
2020-09-02 00:46:59 +0530

    cookis and session added
    passport

--- app.js
+++ app.js
@@ -4,22 +4,47 @@ const express = require("express");
 const bodyParser= require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt=require("bcrypt");
-const saltRounds=10;
+const session = require('express-session');
+const passport = require("passport");
+const passporLocalMongoose = require("passport-local-mongoose");
+
 //const md5 = require("md5");
-mongoose.connect('mongodb://localhost:27017/userBD', {useNewUrlParser: true, useUnifiedTopology: true });
 
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String
-});
 
-const User= mongoose.model("User", userSchema);
+
+
+
+
 
 const app= express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+////////////////////////////////////////////////////////////////////passport1/////////////////////////////////////////////
+app.use(session({
+  secret: "our little secret.",
+  resave : false,
+  saveUninitialized: false
+}));
+////////////////////////////////////////////////////////////////////passport2/////////////////////////////////////////////
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/userBD', {useNewUrlParser: true, useUnifiedTopology: true });
+////////////////////////////////////////////////////////////////////passport3/////////////////////////////////////////////
+mongoose.set('useCreateIndex', true);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+////////////////////////////////////////////////////////////////////passport4/////////////////////////////////////////////
+userSchema.plugin(passporLocalMongoose);
+const User= new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
@@ -33,54 +58,52 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-app.post("/register", function(req, res){
-  //console.log(req.body);
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    // Store hash in your password DB.
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-
-    });
-
-    newUser.save(function(err){
-      if(err) {
-        console.log(err);
-      }
-      else {
-        res.render("secrets");
-      }
-    });
-
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else {
+    res.redirect("/login");
+  }
 });
 
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+})
+
+app.post("/register", function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }
+    else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
 
 });
 
 app.post("/login", function(req, res){
-  const enteredusername= req.body.username;
-  const userpassword= (req.body.password);
-  User.findOne({email: enteredusername}, function(err, foundUser){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function(err){
     if(err){
       console.log(err);
     }
-    else{
-      bcrypt.compare(userpassword, foundUser.password, function(err, result) {
-    // result == true
-        if(result==true){
-            res.render("secrets");
-        }
-        else {
-          console.log("invalid pass");
-        }
-       });
-    }
-      // console.log(enteredusername, " -- 1");
-      // console.log(foundUser.email, " -- 2");
-      // console.log(foundUser.password, " -- 3");
-      // console.log(userpassword, " -- 4");
 
+    else {
+      passport.authenticate("local")(req, res, function(err){
+        res.redirect("/secrets");
+      })
+    }
   });
+
 });
 
 
